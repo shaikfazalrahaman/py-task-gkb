221dabc9d87316a1e7c21af9b5a4e0a4a12c6121
fazal <fazalrahaman186@gmail.com>
2024-07-12 20:09:47 +0530

    Databse creation

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+con = sqlite3.connect('users.db')
+
+def init_db():
+    cs = con.cursor()
+    cs.execute('''
+        Create table if not exists users(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Name TEXT NOT NULL,
+            Email TEXT NOT NULL UNIQUE,
+            Age INTEGER NOT NULL,
+            Date_Of_Birth TEXT NOT NULL
+        )
+    ''')
+    con.commit()
+    con.close()
 
 @app.route('/')
 def init():
